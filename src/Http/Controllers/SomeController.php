4c254859f55c82c0dd859d2ae55a823c2db3a294
reatang <123123>
2021-10-09 19:38:25 +0800

    init comment

--- src/Http/Controllers/SomeController.php
+++ src/Http/Controllers/SomeController.php
@@ -4,10 +4,11 @@ namespace Reatang\LaravelBundle\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Reatang\LaravelBundle\Http\Requests\SomeRequest;
 
 class SomeController extends Controller
 {
-    public function index() {
+    public function index(SomeRequest $request) {
         return view('bundle_name::index', [
             'title' => trans('bundle_name::message.Hello Laravel Bundle')
         ]);
